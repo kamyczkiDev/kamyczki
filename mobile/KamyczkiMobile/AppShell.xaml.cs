d2ba3fa7041dee5016d4022a92881e4c4713afa3
WalterDisneyWhite <mateusz.oborski@gmail.com>
2024-09-24 19:16:06 +0200

    Added simple login page without logic

--- mobile/KamyczkiMobile/AppShell.xaml.cs
+++ mobile/KamyczkiMobile/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace KamyczkiMobile;
+﻿using KamyczkiMobile.Views;
+
+namespace KamyczkiMobile;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
 	}
 }
