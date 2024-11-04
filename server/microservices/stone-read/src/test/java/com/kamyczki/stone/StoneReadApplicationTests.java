54f393f15df3765f943382f09ce7e9514c9018d1
mex1can0 <danetto@wp.pl>
2024-11-04 13:25:03 +0100

    -U option to stone-read

--- server/microservices/stone-read/src/test/java/com/kamyczki/stone/StoneReadApplicationTests.java
+++ server/microservices/stone-read/src/test/java/com/kamyczki/stone/StoneReadApplicationTests.java
@@ -3,7 +3,7 @@ package com.kamyczki.stone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest
 class StoneReadApplicationTests {
 
 	@Test
